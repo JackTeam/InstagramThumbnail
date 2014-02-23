bfd0aadee3dc3a4caba673f8a710e6848accf4ee
xhzengAIB <xhzengAIB@gmail.com>
2014-02-23 15:53:42 +0000

    Add InstagramMediaModel (XHMediaControl) category

---
+++ Source/InstagramMediaModel+XHMediaControl.h
@@ -0,0 +1,13 @@
+//
+//  InstagramMediaModel+XHMediaControl.h
+//  InstagramThumbnail
+//
+//  Created by 曾 宪华 on 14-2-23.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import "InstagramMediaModel.h"
+
+@interface InstagramMediaModel (XHMediaControl)
+
+@end
---
+++ Source/InstagramMediaModel+XHMediaControl.m
@@ -0,0 +1,13 @@
+//
+//  InstagramMediaModel+XHMediaControl.m
+//  InstagramThumbnail
+//
+//  Created by 曾 宪华 on 14-2-23.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import "InstagramMediaModel+XHMediaControl.h"
+
+@implementation InstagramMediaModel (XHMediaControl)
+
+@end
