1fddb2484e8e8feff229de5c1eb78ce785019899
xhzengAIB <xhzengAIB@gmail.com>
2014-02-23 16:02:05 +0000

    Add InstagramCell class

---
+++ Source/InstagramCell.m
@@ -0,0 +1,31 @@
+//
+//  InstagramCell.m
+//  InstagramThumbnail
+//
+//  Created by 曾 宪华 on 14-2-23.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import "InstagramCell.h"
+
+@implementation InstagramCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
