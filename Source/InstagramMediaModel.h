d9a44e23a99edccab8ce70dff4b0118a8a8a5012
xhzengAIB <xhzengAIB@gmail.com>
2014-02-23 15:45:19 +0000

    Add model to media

---
+++ Source/InstagramMediaModel.h
@@ -0,0 +1,13 @@
+//
+//  InstagramMediaModel.h
+//  InstagramThumbnail
+//
+//  Created by 曾 宪华 on 14-2-23.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InstagramMediaModel : NSObject
+
+@end
