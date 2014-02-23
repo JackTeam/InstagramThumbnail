489bd322e9d43126f88fcc702a2c0cd8c560b4d2
xhzengAIB <xhzengAIB@gmail.com>
2014-02-23 16:06:04 +0000

    Design InstagramCell api

--- Source/InstagramCell.h
+++ Source/InstagramCell.h
@@ -7,7 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "InstagramMediaModel.h"
 
 @interface InstagramCell : UICollectionViewCell
+
+@property (nonatomic, strong) UILabel *userLabel;
+@property (nonatomic, strong) UIImageView *userImage;
+@property (nonatomic, strong) UILabel *captionLabel;
+@property (nonatomic, strong) UIButton *imageButton;
+@property (nonatomic, assign) InstagramMediaModel *entity;
+@property (nonatomic, assign) NSIndexPath *indexPath;
+@property (nonatomic, assign) BOOL showOnePicturePerRow;
+
+- (void)setEntity:(InstagramMediaModel *)entity andIndexPath:(NSIndexPath *)index;
 
 @end
