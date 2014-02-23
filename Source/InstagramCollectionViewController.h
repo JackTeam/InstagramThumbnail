1d74345693f63108b4c56b0a41abe485119fd749
xhzengAIB <xhzengAIB@gmail.com>
2014-02-23 15:40:36 +0000

    Add InstagramCollectionViewController class

---
+++ Source/InstagramCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  InstagramCollectionViewController.h
+//  InstagramThumbnail
+//
+//  Created by 曾 宪华 on 14-2-23.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InstagramCollectionViewController : UICollectionViewController
+
+@end
---
+++ Source/InstagramCollectionViewController.m
@@ -0,0 +1,38 @@
+//
+//  InstagramCollectionViewController.m
+//  InstagramThumbnail
+//
+//  Created by 曾 宪华 on 14-2-23.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import "InstagramCollectionViewController.h"
+
+@interface InstagramCollectionViewController ()
+
+@end
+
+@implementation InstagramCollectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
